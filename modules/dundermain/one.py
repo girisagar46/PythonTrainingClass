fc251646a775d5008aeaf21366b52efd9483edae
Sagar Giri <girisagar46@gmail.com>
2018-01-06 14:43:46 +0545

    OOP notebook added

--- modules/dundermain/one.py
+++
@@ -1,8 +0,0 @@
-def my_func():
-	print("hello from my_func")
-
-print("Hello from one.py")
-print("__name__ var in one.py: ", __name__)
-
-if __name__ == "__main__":
-	my_func()
--- modules/helperservice.py
+++
@@ -1,6 +0,0 @@
-def get_farheniet(temp=0):
-    return "{} degree farheniet".format(((float(9)/5)*temp + 32))
-
-
-def get_celsius(temp=0):
-    return "{} degree celsius".format((float(5)/9)*(temp-32))
